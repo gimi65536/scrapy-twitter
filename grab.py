edf52fc949477acae273d2e970767aad12aeaade
Wayne Zeng <wayne@gimi65536.xyz>
2023-06-13 05:48:35 +0000

    Add support for pickle history

--- grab.py
+++ grab.py
@@ -8,7 +8,8 @@ from time import sleep
 from spider import TweetSpider
 from pipeline import TweetPipeline
 
-from json import load, dump
+import json
+import pickle
 from pathlib import Path
 
 from multiprocessing import Process
@@ -16,21 +17,43 @@ from multiprocessing import Process
 with open(config('GRAB_INFO')) as f:
     data = safe_load(f)
 
-history_path = Path('history/history.json')
+use_pickle = config('PICKLE', cast = bool, default = False)
 
-def read_history():
-	if(history_path.exists()):
-		with open(history_path) as f:
-			try:
-				j = load(f)
-			except:
-				pass
-			else:
-				return [set(l) for l in j]
+def load_history():
+	if use_pickle:
+		history_path = Path('history/history.pickle')
+		if(history_path.exists()):
+			with open(history_path, 'rb') as f:
+				try:
+					obj = pickle.load(f)
+				except:
+					pass
+				else:
+					return obj
+	else:
+		history_path = Path('history/history.json')
+		if(history_path.exists()):
+			with open(history_path, 'rt') as f:
+				try:
+					j = json.load(f)
+				except:
+					pass
+				else:
+					return [set(l) for l in j]
 
 	return [set() for _ in data['instance']]
 
-history = read_history()
+def dump_history(history):
+	if use_pickle:
+		history_path = Path('history/history.pickle')
+		with open(history_path, 'wb') as f:
+			pickle.dump(history, f)
+	else:
+		history_path = Path('history/history.json')
+		with open(history_path, 'wt') as f:
+			json.dump([list(s) for s in history], f)
+
+history = load_history()
 
 process = CrawlerProcess(
     settings={
@@ -55,8 +78,7 @@ def run(history):
 	process.start()
 	print("Done")
 	# All processes are done
-	with open(history_path, 'w') as f:
-		dump([list(s) for s in history], f)
+	dump_history(history)
 
 # Main
 while True:
@@ -64,5 +86,5 @@ while True:
 	_process = Process(target = run, args = (history, )) # history will changed in the process but not here
 	_process.start()
 	_process.join()
-	history = read_history() # Update the variable changed in the process
+	history = load_history() # Update the variable changed in the process
 	sleep(data['period'])
